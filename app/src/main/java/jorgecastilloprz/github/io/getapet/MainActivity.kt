408ba2e0a7404908d77c0bdf9b066a71df8b7f2c
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-12-08 12:52:31 +0100

    up to date

--- app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
+++ app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
@@ -1,12 +1,12 @@
 package jorgecastilloprz.github.io.getapet
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
