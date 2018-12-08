8d43d7277e10e0d5252309eb2b7e804c2ca58d86
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-12-08 17:31:08 +0100

    add backdrop

--- app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
+++ app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
@@ -1,12 +1,36 @@
 package jorgecastilloprz.github.io.getapet
 
+import android.os.Build
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import jorgecastilloprz.github.io.getapet.listener.NavigationIconClickListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupActionBar()
+        setupContentBackground()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener(
+            NavigationIconClickListener(
+                this,
+                content,
+                AccelerateDecelerateInterpolator()
+            )
+        )
+    }
+
+    private fun setupContentBackground() {
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            content.background = getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
+    }
 }
