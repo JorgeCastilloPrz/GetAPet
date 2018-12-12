751823a976ba33ec80013ebffe5fe8f4ed8e53ce
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-12-12 15:25:39 +0100

    replace mock pets by more diverse ones

--- app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
+++ app/src/main/java/jorgecastilloprz/github/io/getapet/MainActivity.kt
@@ -62,16 +62,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun mockPets(): List<PetViewState> = listOf(
         PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg"),
-        PetViewState("Lidel", 99.0, "https://www.terranovacnc.com/wp-content/uploads/2017/07/brot-350x350.jpg")
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/1249208/pexels-photo-1249208.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/374825/pexels-photo-374825.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/96428/pexels-photo-96428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/69371/pexels-photo-69371.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/38008/pexels-photo-38008.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/161462/pets-kids-dogs-animal-161462.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/4933/lake-animals-dogs-pets.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/1483185/pexels-photo-1483185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/723130/pexels-photo-723130.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+        PetViewState("Lidel", 99.0, "https://images.pexels.com/photos/326875/pexels-photo-326875.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
     )
 }
