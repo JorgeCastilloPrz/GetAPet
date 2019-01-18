d7ed6783e6792494935fbe846b5a7ae505feaad7
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-18 17:33:29 +0100

    add avatar and scale it down while dragging

--- app/src/main/java/jorgecastilloprz/github/io/getapet/petdetail/PetDetailActivity.kt
+++ app/src/main/java/jorgecastilloprz/github/io/getapet/petdetail/PetDetailActivity.kt
@@ -5,46 +5,58 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.squareup.picasso.Picasso
 import jorgecastilloprz.github.io.getapet.R
+import jorgecastilloprz.github.io.getapet.common.view.picasso.CircleTransform
 import jorgecastilloprz.github.io.getapet.petgrid.picasso.Tint
 import kotlinx.android.synthetic.main.activity_pet_detail.*
 
 class PetDetailActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pet_detail)
-        loadHeaderPicture()
-        setupToolbarIcons()
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_pet_detail)
+    loadHeaderPicture()
+    loadAvatarPicture()
+    setupToolbarIcons()
+  }
 
-    private fun loadHeaderPicture() {
-        val url = intent?.extras?.getString(EXTRA_PET_PICTURE_URL, null)
-        Picasso.get()
-            .load(url)
-            .transform(Tint(1f, ContextCompat.getColor(this, R.color.colorPrimary)))
-            .fit()
-            .centerCrop()
-            .into(details_header)
-    }
+  private fun loadHeaderPicture() {
+    val url = intent?.extras?.getString(EXTRA_PET_PICTURE_URL, null)
+    Picasso.get()
+        .load(url)
+        .transform(Tint(1f, ContextCompat.getColor(this, R.color.colorPrimary)))
+        .fit()
+        .centerCrop()
+        .into(details_header)
+  }
 
-    private fun setupToolbarIcons() {
-        val isDark = intent?.extras?.getBoolean(EXTRA_PET_IS_DARK, false) ?: false
-        val iconRes = if (isDark) R.drawable.ic_close_black_24dp_light else R.drawable.ic_close_black_24dp_dark
-        details_toolbar.setNavigationIcon(iconRes)
+  private fun loadAvatarPicture() {
+    val url = "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=face-facial-hair-fine-looking-614810.jpg&fm=jpg"
+    Picasso.get()
+        .load(url)
+        .transform(CircleTransform())
+        .fit()
+        .centerCrop()
+        .into(avatar)
+  }
 
-        val startingConstraintSet = details_motion.getConstraintSet(R.id.start)
+  private fun setupToolbarIcons() {
+    val isDark = intent?.extras?.getBoolean(EXTRA_PET_IS_DARK, false) ?: false
+    val iconRes = if (isDark) R.drawable.ic_close_black_24dp_light else R.drawable.ic_close_black_24dp_dark
+    details_toolbar.setNavigationIcon(iconRes)
 
-        val darkColor = ContextCompat.getColor(this, R.color.iconColor)
-        val lightColor = ContextCompat.getColor(this, R.color.colorSecondary)
-        startingConstraintSet
-            .getParameters(R.id.details_toolbar)
-            .mCustomConstraints["IconTint"]
-            ?.setColorValue(if (isDark) lightColor else darkColor)
-    }
+    val startingConstraintSet = details_motion.getConstraintSet(R.id.start)
 
-    companion object {
-        const val EXTRA_PET_ID: String = "EXTRA_PET_ID"
-        const val EXTRA_PET_PICTURE_URL: String = "EXTRA_PET_PICTURE_URL"
-        const val EXTRA_PET_IS_DARK: String = "EXTRA_PET_IS_DARK"
-    }
+    val darkColor = ContextCompat.getColor(this, R.color.iconColor)
+    val lightColor = ContextCompat.getColor(this, R.color.colorSecondary)
+    startingConstraintSet
+        .getParameters(R.id.details_toolbar)
+        .mCustomConstraints["IconTint"]
+        ?.setColorValue(if (isDark) lightColor else darkColor)
+  }
+
+  companion object {
+    const val EXTRA_PET_ID: String = "EXTRA_PET_ID"
+    const val EXTRA_PET_PICTURE_URL: String = "EXTRA_PET_PICTURE_URL"
+    const val EXTRA_PET_IS_DARK: String = "EXTRA_PET_IS_DARK"
+  }
 }
